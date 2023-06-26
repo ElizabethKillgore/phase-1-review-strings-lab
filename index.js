ddb85c18e221a82499a8b68681f453189d6277d0
Elizabeth <ek@elizabethkillgore.com>
2023-06-26 15:56:10 -0500

    pass Strings lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+const currentUser = 'Liz Smith'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+
+
+
+
+
+
+
+
+
+
